20b0a3dfac545b08517197a94755f4c75e271349
vincent <vin.beck24@gmail.com>
2026-09-10 15:36:58 +0200

    add temporary themes

---
+++ themes/light_theme.gd
@@ -0,0 +1,38 @@
+extends Node
+static var theme = ColorScheme.new(
+Color("#3B693A"), # primary
+Color("#FFFFFF"), # onPrimary
+Color("#BCF0B5"), # primaryContainer
+Color("#235024"), # onPrimaryContainer
+Color("#52634F"), # secondary
+Color("#FFFFFF"), # onSecondary
+Color("#D5E8CF"), # secondaryContainer
+Color("#3B4B38"), # onSecondaryContainer
+Color("#38656A"), # tertiary
+Color("#FFFFFF"), # onTertiary
+Color("#BCEBF1"), # tertiaryContainer
+Color("#1F4D52"), # onTertiaryContainer
+Color("#BA1A1A"), # error
+Color("#FFFFFF"), # onError
+Color("#FFDAD6"), # errorContainer
+Color("#93000A"), # onErrorContainer
+Color("#F7FBF1"), # background
+Color("#181D17"), # onBackground
+Color("#F7FBF1"), # surface
+Color("#181D17"), # onSurface
+Color("#DEE5D8"), # surfaceVariant
+Color("#424940"), # onSurfaceVariant
+Color("#72796F"), # outline
+Color("#C2C9BD"), # outlineVariant
+Color("#000000"), # scrim
+Color("#2D322C"), # inverseSurface
+Color("#EEF2E9"), # inverseOnSurface
+Color("#A0D39A"), # inversePrimary
+Color("#D8DBD2"), # surfaceDim
+Color("#F7FBF1"), # surfaceBright
+Color("#FFFFFF"), # surfaceContainerLowest
+Color("#F1F5EC"), # surfaceContainerLow
+Color("#ECEFE6"), # surfaceContainer
+Color("#E6E9E0"), # surfaceContainerHigh
+Color("#E0E4DB"), # surfaceContainerHighest
+)
