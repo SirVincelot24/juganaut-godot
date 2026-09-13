317cb6ac38ff9fa405ecc3aefbde935fd8d57686
vincent <vin.beck24@gmail.com>
2026-09-13 15:35:54 +0200

    make the settings menu grab focus on opening it

--- scripts/UI/buttons/settings.gd
+++ scripts/UI/buttons/settings.gd
@@ -1,7 +1,9 @@
 extends Button
 
 func _pressed() -> void:
-	$/root/SettingsMenu.visible = true
+	var settings_menu = $/root/SettingsMenu 
+	settings_menu.visible = true
+	settings_menu.grab_focus()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("main_menu_settings"):
