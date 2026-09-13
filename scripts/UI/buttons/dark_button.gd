3b0ed697d5f3c3eb8c96dc2fba31b4b5dc8c09ef
vincent <vin.beck24@gmail.com>
2026-09-13 14:00:35 +0200

    save dark mode in player prefs

--- scripts/UI/buttons/dark_button.gd
+++ scripts/UI/buttons/dark_button.gd
@@ -1,4 +1,10 @@
 extends CheckButton
 
-func _toggled(_toggled_on: bool) -> void:
-	ThemeManager.toggle_theme()
+func _toggled(toggled_on: bool) -> void:
+	ThemeManager.set_dark_mode(toggled_on)
+	PlayerPrefs.set_pref("dark_mode", toggled_on)
+	
+func _ready() -> void:
+	var is_dark_mode = PlayerPrefs.get_bool("dark_mode", true)
+	_toggled(is_dark_mode)
+	button_pressed = is_dark_mode
