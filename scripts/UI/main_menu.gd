902dfbd2426507b472d2d9f4d5c9d4361ab3a77e
vincent <vin.beck24@gmail.com>
2026-09-15 15:07:51 +0200

    add dynamic background switching

---
+++ scripts/UI/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+var portrait_bg = preload("res://textures/menu-background-phone.png")
+var landscape_bg = preload("res://textures/menu-background.png")
+
+func update_background():
+	var bg_box = $Background.get_theme_stylebox("panel")
+	if get_viewport().get_visible_rect().size.aspect() > 1:
+		bg_box.texture = landscape_bg
+	else :
+		bg_box.texture = portrait_bg
+
+func _ready() -> void:
+	get_viewport().size_changed.connect(update_background)
